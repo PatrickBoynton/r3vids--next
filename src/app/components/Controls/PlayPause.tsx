7308dc8f21567f5ea618143efc6264fddf3cfc6b
Patrick Boynton <youremail@yourdomain.com>
2024-06-07 19:56:42 -0400

    Updated the useEffects

--- src/app/components/Controls/PlayPause.tsx
+++ src/app/components/Controls/PlayPause.tsx
@@ -2,20 +2,34 @@
 import { Pause, PlayArrow } from "@mui/icons-material"
 import { useVideoControlsStore } from "@/stores/videoControlsStore"
 import { useVideoStore } from "@/stores/videoStore"
-import { useState } from "react"
+import { useEffect } from "react"
 
 export const PlayPause = () => {
-	const { isPlaying, setIsPlaying } = useVideoControlsStore()
-	const [hasStopped, setHasStopped] = useState(false)
+	const { isPlaying, setIsPlaying, setIsMuted } = useVideoControlsStore()
 	const { updateVideo, randomVideo } = useVideoStore()
 	const { vidRef } = useVideoControlsStore()
+	const isPaused = vidRef?.current?.paused
+
+	useEffect(() => {
+		setIsMuted(false)
+	}, [setIsMuted])
 
 	const handleClick = async () => {
 		setIsPlaying(!isPlaying)
+
+		if (!vidRef.current) return
+
 		const currentTime = vidRef?.current?.currentTime
 
-		if (isPlaying) {
+		if (isPlaying && !vidRef.current?.paused) {
+			vidRef.current?.addEventListener("pause", () => {
+				setIsPlaying(false)
+			})
+			vidRef.current.addEventListener("webkitendfullscreen", () => {
+				setIsPlaying(false)
+			})
 			vidRef.current?.pause()
+
 			if (randomVideo !== null) {
 				if (currentTime !== undefined) {
 					randomVideo.videoStatus.currentPlayTime = currentTime
@@ -23,14 +37,16 @@ export const PlayPause = () => {
 				}
 			}
 		} else {
+			vidRef.current?.addEventListener("play", () => {
+				setIsPlaying(true)
+			})
 			await vidRef.current?.play()
-			setIsPlaying(!isPlaying)
 		}
 	}
 
 	return (
 		<button id="play-pause" onClick={handleClick}>
-			{!hasStopped && !isPlaying ? <PlayArrow /> : <Pause />}
+			{!isPlaying && isPaused ? <PlayArrow /> : <Pause />}
 		</button>
 	)
 }
