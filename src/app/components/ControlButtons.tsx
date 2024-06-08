a0d7f61b4fe687022d34f3c4e85d9bd4125ab2ae
Patrick Boynton <youremail@yourdomain.com>
2024-06-07 20:11:12 -0400

    Removed controls, updated the useeffect.

--- src/app/components/ControlButtons.tsx
+++ src/app/components/ControlButtons.tsx
@@ -1,12 +1,18 @@
+"use client"
 import { PlayPause } from "@/app/components/Controls/PlayPause"
 import { Random } from "@/app/components/Controls/Random"
 import { RandomPlayed } from "@/app/components/Controls/RandomPlayed"
-import { Timer } from "@/app/components/Controls/Timer"
 import { Volume } from "@/app/components/Controls/Volume"
 import { FullScreen } from "@/app/components/Controls/FullScreen"
 import { ResetVideoStatus } from "@/app/components/Controls/ResetVideoStatus"
+import { useVideoStore } from "@/stores/videoStore"
+import { useEffect } from "react"
+import AlarmAddIcon from "@mui/icons-material/AlarmAdd"
+import { Timer } from "@/app/components/Controls/Timer"
 
 export const ControlButtons = () => {
+	const { randomVideo } = useVideoStore()
+	useEffect(() => {}, [randomVideo])
 	return (
 		<div className="video-controls">
 			<PlayPause />
@@ -16,6 +22,9 @@ export const ControlButtons = () => {
 			<Timer />
 			<Volume />
 			<FullScreen />
+			<button id="watch-later">
+				<AlarmAddIcon />
+			</button>
 		</div>
 	)
 }
