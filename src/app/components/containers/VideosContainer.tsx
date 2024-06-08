67f478ccf6e54e5cf789c2bea4137c121f9ebd5a
Patrick Boynton <youremail@yourdomain.com>
2024-06-07 20:07:21 -0400

    Reorganizing and updates.

--- src/app/components/containers/VideosContainer.tsx
+++ src/app/components/containers/VideosContainer.tsx
@@ -5,7 +5,6 @@ import {
 	convertDuration,
 	handleClick,
 } from "@/app/components/containers/sharedFunctions"
-import AlarmAddIcon from "@mui/icons-material/AlarmAdd"
 import { useEffect } from "react"
 
 type Props = {
@@ -13,64 +12,43 @@ type Props = {
 }
 
 export const VideosContainer = ({ type }: Props) => {
-	const {
-		videos,
-		setVideos,
-		setPlayedVideos,
-		setSrc,
-		setTitle,
-		updateVideo,
-		playedVideos,
-	} = useVideoStore()
+	const { videos, setPlayedVideos, playedVideos, randomVideo, setVideos } =
+		useVideoStore()
 
 	const whatType = type === "played" ? playedVideos : videos
 
+	const sortedVideos = () =>
+		whatType.sort((a, b) => {
+			if (type === "videos") {
+				return b.videoStatus.playCount - a.videoStatus.playCount
+			} else {
+				const timeA = new Date(a.videoStatus.lastPlayed).getTime()
+				const timeB = new Date(b.videoStatus.lastPlayed).getTime()
+				return timeB - timeA
+			}
+		})
+
 	useEffect(() => {
 		setPlayedVideos()
-	}, [setPlayedVideos, videos])
+	}, [setPlayedVideos, randomVideo])
 
 	return (
 		<div className={`${type}-container`}>
-			{whatType
-				.sort((a, b) => {
-					if (type === "videos") {
-						return b.videoStatus.playCount - a.videoStatus.playCount
-					} else {
-						const timeA = new Date(
-							a.videoStatus.lastPlayed
-						).getTime()
-						const timeB = new Date(
-							b.videoStatus.lastPlayed
-						).getTime()
-						return timeB - timeA
-					}
-				})
-				.map((video: Video) => (
-					<div
-						className="videos"
-						key={video.id}
-						onClick={() =>
-							handleClick(
-								video,
-								setSrc,
-								setTitle,
-								updateVideo,
-								setVideos
-							)
-						}>
-						<div className={`${type}-container`}>
-							<div className="source">
-								<h3>{video.title}</h3>
-								<p>{video.videoStatus.playCount}</p>
-								<p>{convertDuration(video.duration)}</p>
-								<p>{video.url}</p>
-							</div>
-							<button id="watch-later">
-								<AlarmAddIcon />
-							</button>
+			{sortedVideos().map((video: Video) => (
+				<div
+					className="videos"
+					key={video.id}
+					onClick={() => handleClick(video)}>
+					<div className={`${type}-container`}>
+						<div className="source">
+							<h3>{video.title}</h3>
+							<p>{video.videoStatus.playCount}</p>
+							<p>{convertDuration(video.duration)}</p>
+							<p>{video.url}</p>
 						</div>
 					</div>
-				))}
+				</div>
+			))}
 		</div>
 	)
 }
