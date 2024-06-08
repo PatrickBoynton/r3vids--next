71f808e527dfbcecc4e5bc260ea73655030c4496
Patrick Boynton <youremail@yourdomain.com>
2024-06-07 20:07:47 -0400

    Added duration and currentDuration.

--- src/app/components/Controls/Timer.tsx
+++ src/app/components/Controls/Timer.tsx
@@ -1,7 +1,17 @@
+"use client"
+import { useVideoStore } from "@/stores/videoStore"
+import { convertDuration } from "@/app/components/containers/sharedFunctions"
+
 export const Timer = () => {
+	const { randomVideo, currentVideo } = useVideoStore()
+	const randomVid = randomVideo?.videoStatus?.currentPlayTime as number
+	const currentVid = currentVideo?.videoStatus?.currentPlayTime as number
+
 	return (
 		<>
+			{convertDuration(randomVid || currentVid)}
 			<input type="range" id="seek-bar" value="0" onChange={() => {}} />
+			{convertDuration(randomVid || currentVid)}
 		</>
 	)
 }
