d3c12321a85733bdbb1e45c9d99f6216e5398bba
Patrick Boynton <youremail@yourdomain.com>
2024-06-23 20:59:36 -0400

    Moved the handleRandomClick outside the file.

--- src/app/components/Controls/Random.tsx
+++ src/app/components/Controls/Random.tsx
@@ -1,15 +1,7 @@
+import { handleRandomClick } from "@/app/components/containers/sharedFunctions"
 import { Shuffle } from "@mui/icons-material"
-import { useVideoStore } from "@/stores/videoStore"
 
 export const Random = () => {
-	const { setRandomVideo, setVideos, query, value } = useVideoStore()
-
-	const handleRandomClick = () => {
-		localStorage.removeItem("currentVideo")
-		setRandomVideo(query, value)
-		setVideos()
-	}
-
 	return (
 		<button id="random" onClick={() => handleRandomClick()}>
 			<Shuffle />
