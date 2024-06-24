c6d0db916384dcc8d855e6c52d26daabb519cb80
Patrick Boynton <youremail@yourdomain.com>
2024-06-23 20:57:44 -0400

    Changed from CSS to tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#201449",
+        secondary: "#FFDB60",
+      },
+    },
+  },
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        "*": {
+          boxSizing: "border-box",
+          margin: 0,
+          padding: 0,
+          fontSize: "16px",
+          borderColor: "#FFDB60",
+        },
+        h1: {
+          fontSize: "3rem",
+        },
+        h3: {
+          fontSize: "1.5rem",
+        },
+        svg: {
+          width: "35px !important",
+          height: "35px !important",
+        },
+      });
+    },
+  ],
+};
