e319d672a2fed42baee1ab398cd6db1ab5cb820b
Patrick Boynton <youremail@yourdomain.com>
2024-06-23 21:00:33 -0400

    Created a file for functions related to components.

--- src/app/components/containers/sharedFunctions.ts
+++ src/app/components/containers/sharedFunctions.ts
@@ -1,34 +1,35 @@
 import { Video } from "@/types"
+import { useVideoStore } from "@/stores/videoStore"
 
-export const handleClick = (
-	video: Video,
-	setSrc: (src: string) => void,
-	setTitle: (title: string) => void,
-	updateVideo: (video: Video) => void,
-	setVideos: () => void
-) => {
-	setSrc(video.url)
-	setTitle(video.title)
+export const handleClick = (video?: Video) => {
+	console.log("video", video)
+	if (video) {
+		useVideoStore.getState().setUrl(video.url)
+		useVideoStore.getState().setTitle(video.title)
 
-	video.videoStatus.playCount++
-	video.videoStatus.lastPlayed = new Date().toISOString()
-	video.videoStatus.played = true
-
-	updateVideo(video)
-	setVideos()
-	const currentVideo = JSON.parse(
-		localStorage.getItem("currentVideo") as string
-	)
-
-	if (currentVideo) {
-		localStorage.setItem("currentVideo", JSON.stringify(video))
-	} else {
-		localStorage.removeItem("currentVideo")
+		useVideoStore.getState().updateVideo(video)
+		useVideoStore.getState().setVideos()
+		useVideoStore.getState().setPlayedVideos()
+		// useVideoStore.getState().setCurrentVideo(video)
 	}
+}
+
+export const handleRandomClick = async () => {
+	useVideoStore.getState().setRandomVideo()
+
+	const randomVideo = useVideoStore.getState().randomVideo as Video
+	if (randomVideo) {
+		useVideoStore.getState().setUrl(randomVideo.url)
+		useVideoStore.getState().setTitle(randomVideo.title)
+	}
+
+	useVideoStore.getState().setVideos()
+	useVideoStore.getState().setPlayedVideos()
+	// useVideoStore.getState().setCurrentVideo(randomVideo)
 }
 
 export const convertDuration = (duration: number) => {
 	const minutes = Math.floor(duration / 60)
 	const seconds = duration - minutes * 60
-	return `${minutes}:${seconds.toFixed(0)}`
+	return duration ? `${minutes}:${seconds.toFixed(2)}` : "00:00"
 }
