f5db230fbab10a724073a01cd324e9617722e109
Patrick Boynton <youremail@yourdomain.com>
2024-06-23 21:01:43 -0400

    Working on updating the way the time is displayed.

--- src/app/components/Controls/Timer.tsx
+++ src/app/components/Controls/Timer.tsx
@@ -4,14 +4,17 @@ import { convertDuration } from "@/app/components/containers/sharedFunctions"
 
 export const Timer = () => {
 	const { randomVideo, currentVideo } = useVideoStore()
-	const randomVid = randomVideo?.videoStatus?.currentPlayTime as number
-	const currentVid = currentVideo?.videoStatus?.currentPlayTime as number
+	const randomVid = randomVideo?.duration as number
+	// const currentVid = currentVideo?.duration as number
+	// const currentPlayTime = currentVideo?.videoStatus.currentPlayTime as number
+	const currentPlayTimeRandom = randomVideo?.videoStatus
+		.currentPlayTime as number
 
 	return (
 		<>
-			{convertDuration(randomVid || currentVid)}
+			{convertDuration(currentPlayTimeRandom)}
 			<input type="range" id="seek-bar" value="0" onChange={() => {}} />
-			{convertDuration(randomVid || currentVid)}
+			{convertDuration(randomVid)}
 		</>
 	)
 }
